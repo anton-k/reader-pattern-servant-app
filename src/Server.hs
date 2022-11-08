0d598b3970c7a40b762aa0e4ef0f5cbe7c298c73
Anton Kholomiov <a.kholomyev@tinkoff.ru>
2022-11-08 18:08:14 +0300

    Simplify servant error pass

--- src/Server.hs
+++ src/Server.hs
@@ -26,10 +26,8 @@ server env =
   where
     onRequest :: (req -> App env resp) -> env -> req -> Servant.Handler resp
     onRequest handler e req = do
-      eResp <- liftIO $ runApp (handler req) e
-      case eResp of
-        Right resp -> pure resp
-        Left err   -> throwError $ err400 { errBody = BL.fromStrict $ Text.encodeUtf8 err }
+      eResp <- liftIO (runApp (handler req) e)
+      either toServantError pure eResp
 
     saveEnv    = SaveEnv { log = addContext "api.save" env.log, db = env.db, time = env.time }
 
@@ -40,4 +38,6 @@ server env =
     toggleLogEnv = ToggleLogEnv { log = addContext "api.toggle-log" env.log }
 
     addContext namespace = mapILog (addLogContext namespace)
+
+    toServantError err = throwError $ err400 { errBody = BL.fromStrict $ Text.encodeUtf8 err }
 
