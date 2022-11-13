8c610b02cbf40d7cddfd7d684205f5161facfe2c
Anton Kholomiov <a.kholomyev@tinkoff.ru>
2022-11-13 13:14:38 +0300

    simplify env

--- src/App.hs
+++ src/App.hs
@@ -4,15 +4,18 @@ module App
   , ApiError (..)
   , throwApi
   , runApp
+  , toHandler
+  , toHandler1
   , module X
   ) where
 
 import Data.Text (Text)
+import Control.Exception (try)
 import Control.Monad.IO.Class as X
 import Control.Monad.Reader as X
 import Control.Monad.Except as X
 import Control.Monad.Catch as X (MonadThrow(..), MonadCatch(..))
-import Servant.Server
+import Servant.Server as Servant
 import Data.ByteString.Lazy   qualified as BL
 import Data.Text.Encoding     qualified as Text
 
@@ -24,10 +27,17 @@ newtype App env a = App (ReaderT env IO a)
     , MonadReader env
     , MonadIO
     , MonadThrow
+    , MonadCatch
     )
 
 runApp :: App env a -> env -> IO a
 runApp (App a) env = runReaderT a env
+
+toHandler :: env -> App env resp -> Servant.Handler resp
+toHandler e handler = Handler $ ExceptT $ try $ runApp handler e
+
+toHandler1 :: env -> (req -> App env resp) -> req -> Servant.Handler resp
+toHandler1 env handle a = toHandler env (handle a)
 
 newtype ApiError = ApiError Text
 
@@ -35,5 +45,4 @@ throwApi :: ApiError -> App env a
 throwApi = throwM . toServantError
   where
     toServantError (ApiError err) = err400 { errBody = BL.fromStrict $ Text.encodeUtf8 err }
-
 
